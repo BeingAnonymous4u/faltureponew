4c0533ab0606701756588b5075c4c86db6195004
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-08-02 12:35:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -771,10 +771,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ⇆', url=f'http://t.me/{temp.U_NAME}?startgroup=start')
         ],[
             InlineKeyboardButton('⚙ ꜰᴇᴀᴛᴜʀᴇs', callback_data='features'),
-            InlineKeyboardButton('🌹 ᴀʙᴏᴜᴛ ', callback_data=f'about'),
+            InlineKeyboardButton('👩‍🌾 ᴀʙᴏᴜᴛ 🍃', callback_data=f'about'),
 	],
         [
-            InlineKeyboardButton('🎖️ ʏᴏᴜʀ ᴘᴏɪɴᴛs 🎖️', callback_data=f'point#{query.from_user.id}'),
+            InlineKeyboardButton('🎋 ʏᴏᴜʀ ᴘᴏɪɴᴛs ✨', callback_data=f'point#{query.from_user.id}'),
             InlineKeyboardButton('💰 ᴘʀᴇᴍɪᴜᴍ 💎', callback_data='premium')
         ],
         [
