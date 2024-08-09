033ab79aab307a6f676c510ca53d7c59ee06a03c
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-08-09 14:01:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -601,13 +601,14 @@ async def lang_search(client: Client, query: CallbackQuery):
         
 
     btn.insert(0,[
-        InlineKeyboardButton("♻️ sᴇɴᴅ ᴀʟʟ", callback_data=batch_link),
-        InlineKeyboardButton("🎭 ʟᴀɴɢᴜᴀɢᴇ", callback_data=f"languages#{key}#{offset}#{req}"),
+        InlineKeyboardButton("🎭 ᴄʜᴏᴏsᴇ ǫᴜᴀʟɪᴛʏ ✨", callback_data=f"qualities#{key}#{offset}#{req}"),
         ])
     btn.insert(1, [
-        InlineKeyboardButton("🔥ǫᴜᴀʟɪᴛʏ ", callback_data=f"qualities#{key}#{offset}#{req}"),
-        InlineKeyboardButton(" ʏᴇᴀʀ❗", callback_data=f"years#{key}#{offset}#{req}"),
-        InlineKeyboardButton(" ꜱᴇᴀꜱᴏɴ🍿 ", callback_data=f"seasons#{key}#{offset}#{req}"),
+        InlineKeyboardButton("🚩 ʏᴇᴀʀ ⌛", callback_data=f"years#{key}#{offset}#{req}"),
+        InlineKeyboardButton("✨ season🍿", callback_data=f"seasons#{key}#{offset}#{req}")
+    ])
+    btn.insert(2, [
+        InlineKeyboardButton("♻️ sᴇɴᴅ ᴀʟʟ", callback_data=batch_link),
     ])
     if n_offset== '':
         btn.append(
